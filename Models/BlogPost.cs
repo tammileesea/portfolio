606ea9a18671e20cca7673badfbeb90e1d54d8a8
Tammi Lee <tammilee219@gmail.com>
2023-10-06 13:01:30 -0700

    adding blog post class

---
+++ Models/BlogPost.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace tamDevMvc.Models
+{
+	public class BlogPost
+	{
+		public string Title { get; set; }
+		public DateTime CreatedAt { get; set; }
+		public DateTime UpdatedAt { get; set; }
+
+		public BlogPost()
+		{
+		}
+	}
+}
+
